3a92420c24c54b5fde8223218bb09c884232a3cc
geek_inside <38232156+bizvip@users.noreply.github.com>
2023-04-28 01:09:34 +0700

    1231231

---
+++ src/views/channel/revenue/index.vue
@@ -0,0 +1,121 @@
+<template>
+    <div class="ma-content-block lg:flex justify-between p-4">
+        <ma-crud :options="options" :columns="columns" ref="crudRef">
+        </ma-crud>
+    </div>
+</template>
+<script setup>
+import { reactive, ref } from 'vue'
+import channelRevenue from '@/api/channel/channelRevenue'
+
+const crudRef = ref()
+
+const options = reactive({
+  id: 'cm_channel_revenue',
+  rowSelection: false,
+  pk: 'id',
+  operationColumn: false,
+  operationWidth: 160,
+  formOption: {
+    viewType: 'modal',
+    width: 600
+  },
+  api: channelRevenue.getList,
+  import: {
+    show: true,
+    url: 'channel/revenue/import',
+    templateUrl: 'channel/revenue/downloadTemplate',
+    auth: ['channel:revenue:import']
+  },
+  export: {
+    show: true,
+    url: 'channel/revenue/export',
+    auth: ['channel:revenue:export']
+  }
+})
+
+const columns = reactive([
+  {
+    title: 'ID',
+    dataIndex: 'id',
+    formType: 'input',
+    addDisplay: false,
+    editDisplay: false
+  },
+  {
+    title: '统计日期',
+    dataIndex: 'date',
+    formType: 'range',
+    search: true,
+    addDisplay: false,
+    editDisplay: false,
+    showTime: false
+  },
+  {
+    title: '渠道ID',
+    dataIndex: 'channel_id',
+    formType: 'input',
+    search: false,
+    addDisplay: false,
+    editDisplay: false
+  },
+  {
+    title: '产品ID',
+    dataIndex: 'product_id',
+    formType: 'input',
+    search: false,
+    addDisplay: false,
+    editDisplay: false
+  },
+  {
+    title: '渠道名字',
+    dataIndex: 'channel_name',
+    formType: 'input',
+    search: false,
+    addDisplay: false,
+    editDisplay: false
+  },
+  {
+    title: '产品名字',
+    dataIndex: 'product_name',
+    formType: 'input',
+    search: false,
+    addDisplay: false,
+    editDisplay: false
+  },
+  {
+    title: '收益金额',
+    dataIndex: 'amount',
+    formType: 'input',
+    addDisplay: false,
+    editDisplay: false
+  },
+  {
+    title: '创建权限',
+    dataIndex: 'created_by',
+    formType: 'input',
+    addDisplay: false,
+    editDisplay: false,
+    hide: true
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'created_at',
+    formType: 'input',
+    addDisplay: false,
+    editDisplay: false,
+    mode: 'date',
+    showTime: true
+  },
+  {
+    title: '更新时间',
+    dataIndex: 'updated_at',
+    formType: 'input',
+    addDisplay: false,
+    editDisplay: false,
+    mode: 'date',
+    showTime: true
+  }
+])
+</script>
+<script> export default { name: 'channel:revenue' } </script>
---
+++ src/views/settlement/placement/index.vue
@@ -0,0 +1,162 @@
+<template>
+    <div class="ma-content-block lg:flex justify-between p-4">
+        <ma-crud :options="options" :columns="columns" ref="crudRef" table-layout-fixed="table-layout-fixed">
+        </ma-crud>
+    </div>
+</template>
+<script setup>
+import { reactive, ref } from 'vue'
+import settlementPlacement from '@/api/settlement/settlementPlacement'
+
+const crudRef = ref()
+
+const options = reactive({
+  id: 'cm_settlement_placement',
+  rowSelection: false,
+  pk: 'id',
+  operationColumn: false,
+  operationWidth: 160,
+  formOption: {
+    viewType: 'modal',
+    width: 600
+  },
+  api: settlementPlacement.getList,
+  import: {
+    show: true,
+    url: 'settlement/placement/import',
+    templateUrl: 'settlement/placement/downloadTemplate',
+    auth: ['settlement:placement:import']
+  },
+  export: {
+    show: true,
+    url: 'settlement/placement/export',
+    auth: ['settlement:placement:export']
+  }
+})
+
+const columns = reactive([
+  {
+    title: 'ID',
+    dataIndex: 'id',
+    formType: 'input',
+    addDisplay: false,
+    editDisplay: false
+  },
+  {
+    title: '渠道ID',
+    dataIndex: 'channel_id',
+    formType: 'cascader',
+    search: true,
+    addDisplay: false,
+    editDisplay: false,
+    hide: true
+  },
+  {
+    title: '渠道标识',
+    dataIndex: 'channel_slug',
+    formType: 'input',
+    search: true,
+    addDisplay: false,
+    editDisplay: false
+  },
+  {
+    title: '产品名字',
+    dataIndex: 'product_name',
+    formType: 'cascader',
+    search: true,
+    addDisplay: false,
+    editDisplay: false,
+    multiple: false
+  },
+  {
+    title: '产品ID',
+    dataIndex: 'product_id',
+    formType: 'input',
+    search: true,
+    addDisplay: false,
+    editDisplay: false,
+    hide: true
+  },
+  {
+    title: '订单金额',
+    dataIndex: 'order_amount',
+    formType: 'input',
+    addDisplay: false,
+    editDisplay: false
+  },
+  {
+    title: '支付金额',
+    dataIndex: 'pay_amount',
+    formType: 'input',
+    addDisplay: false,
+    editDisplay: false
+  },
+  {
+    title: '订单号',
+    dataIndex: 'order_no',
+    formType: 'input',
+    search: true,
+    addDisplay: false,
+    editDisplay: false
+  },
+  {
+    title: '支付时间',
+    dataIndex: 'pay_time',
+    formType: 'range',
+    search: true,
+    addDisplay: false,
+    editDisplay: false,
+    showTime: true
+  },
+  {
+    title: 'UUID',
+    dataIndex: 'uuid',
+    formType: 'input',
+    search: true,
+    addDisplay: false,
+    editDisplay: false
+  },
+  {
+    title: '状态',
+    dataIndex: 'state',
+    formType: 'select',
+    search: true,
+    addDisplay: false,
+    editDisplay: false,
+    multiple: false
+  },
+  {
+    title: '客户端用户ID',
+    dataIndex: 'client_user_id',
+    formType: 'input',
+    addDisplay: false,
+    editDisplay: false
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'created_at',
+    formType: 'range',
+    addDisplay: false,
+    editDisplay: false,
+    showTime: true
+  },
+  {
+    title: '更新时间',
+    dataIndex: 'updated_at',
+    formType: 'date',
+    addDisplay: false,
+    editDisplay: false,
+    hide: true,
+    showTime: true
+  },
+  {
+    title: '数据权限',
+    dataIndex: 'created_by',
+    formType: 'input',
+    addDisplay: false,
+    editDisplay: false,
+    hide: true
+  }
+])
+</script>
+<script> export default { name: 'settlement:placement' } </script>
